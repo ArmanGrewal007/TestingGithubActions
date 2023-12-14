0897ee225a2b3b2c3377274da0d07d89df50ae8c
ArmanGrewal007 <armansinghgrewal@gmail.com>
2023-12-15 00:10:27 +0530

    added ruff

---
+++ arman.py
@@ -0,0 +1,19 @@
+
+
+class Test:
+    def __init__(self):
+        self.__num = 100
+
+    def setNum(self, newNum):
+        self.__num = newNum
+
+    def getNum(self):
+        return self.__num
+
+if __name__ == '__main__':
+    t = Test()
+    print(t.getNum())
+    t.setNum(50)
+    print(t.getNum())
+    # print(t.__num)  # AttributeError: 'Test' object has no attribute '__num'
+    print(t._Test__num)  # 50
